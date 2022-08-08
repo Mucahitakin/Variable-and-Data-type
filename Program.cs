21b3f8360565de01e993d0f92dd82d182cb0b536
Akin_m <mucahitakn37@gmail.com>
2022-08-09 02:00:48 +0300

    c#

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+// int deger = 2;
+// string degisken = null;
+// string Degisken=null;
+// string degisken_deger=null;
+// string degisken veri=null;
+//string degisken=" ";
+// byte b=5; 1byte
+// sbyte c =5; 1byte
+
+// short s=5;   2byte
+// ushort us=5; byte
+
+// int i16=2;     2byte
+// int i=2;       4byte
+// int i64=2;     8byte
+// uint ui=2;     4byte
+// long l=4;      8byte
+// float f=5;     4byte
+// double d=5     8byte
+// decimal de=5;  16byte
+// char c='2';    2byte
+// string str='Mücahit';  sınırsız
+// bool b1=true;
+// bool b2=false;
+// DateTime dt = DateTime.Now;
+// Console.WriteLine(dt);
+
+// object o1 = "x";
+// object o2 = 'y';
+// object o3 = 4;
+// object o4 = 4.3;
+
+// string str = "";
+// string str1 = string.Empty;
+// str1 = "Muco";
+// string ad = "Muco";
+// string soyad = "Akin";
+// Console.WriteLine(ad + " " + soyad);
+// string tamIsim= ad+ " "+soyad;
+
+
+// int integer1=5;
+// int integer2=3;
+// int integer3=integer1*integer2;
+
+// bool b1 = 10 < 2;
+// bool b2 = 5 < 2;
+
+
+// string str30 = "30";
+// int int20 = 20;
+// string yeniDeger = str30 + int20.ToString();
+
+// int int21 = int20 + Convert.ToInt32(str30);
+// int int22 = int20 + int.Parse(str30);
+
+// string datetime = DateTime.Now.ToString("DD.MM.YYYY");
+// Console.WriteLine(datetime);
+// string datetime2 = DateTime.Now.ToString("DD/MM/YYYY");
+// Console.WriteLine(datetime2);
+// string datetime3 = DateTime.Now.ToString("HH:MM");
+// Console.WriteLine(datetime3);
+// Console.WriteLine(" ");
+
